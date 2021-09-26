a15c76ee4ee0014bef9a36effe145efa403e406c
stgeorgiev7 <stgeorgiev7@gmail.com>
2021-09-26 20:41:55 +0300

    added notes, set to localStorage, get after reload

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import "./App.css";
+import React, {useState} from "react";
 
 function App() {
+  const value = localStorage.getItem('coolNote');
+
+  const [textArea, setText] = useState(value? value: '');
+  
   return (
     <div className="App">
       <div className="box">
         <div className="field">
           <div className="control">
-            <textarea className="textarea is-large" placeholder="Notes..." />
+            <textarea
+            className="textarea is-large" 
+            placeholder="Notes..."
+            value={textArea} 
+            onChange={(e) => {
+              setText(e.target.value);
+            }} />
           </div>
         </div>
-        <button className="button is-large is-primary is-active">Save</button>
-        <button className="button is-large">Clear</button>
+        <button className="button is-large is-primary is-active" onClick={() => {
+          localStorage.setItem('coolNote', textArea);
+        }}>Save</button>
+        <button className="button is-large" onClick={() => {setText('');}}>Clear</button>
       </div>
     </div>
   );
